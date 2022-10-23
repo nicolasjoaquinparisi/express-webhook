edf9dd5de2a7e4ef779f640d2db862a120e23a08
NicolasJoaquinParisi <nicojp.1808@gmail.com>
2022-10-23 20:20:33 -0300

    added new env configs. added example env

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.use(morgan('dev'));
 
 const PORT = process.env.PORT || 3000;
 const TARGET = process.env.TARGET;
+const METHOD = process.env.METHOD;
+const ORIGIN = process.env.USE_REQUEST_ORIGIN;
+const SEND_DATA = process.env.SEND_DATA;
 
 app.get('/hook/healthcheck', (req, res) => {
   res.status(200).json({ msg: 'Webhook online' });
@@ -22,19 +25,25 @@ app.get('/hook/healthcheck', (req, res) => {
 
 app.post('/hook', async (req, res) => {
   if (!TARGET)
-    return res.status(501).send({ msg: 'There is not target url setted' });
+    return res.status(501).send({ msg: 'There is no target url setted' });
+  if (!METHOD)
+    return res.status(501).send({ msg: 'There is no http method setted' });
 
-  const payload = { ...req.body };
-
-  const response = await fetch(TARGET, {
-    method: 'POST',
+  const requestObject = {
+    method: METHOD,
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(payload),
-  });
+  };
 
-  return res.status(200).send(response);
+  if (ORIGIN && req.headers.origin)
+    requestObject.headers['Origin'] = req.headers.origin;
+
+  if (SEND_DATA) requestObject.body = JSON.stringify({ ...req.body });
+
+  const response = await fetch(TARGET, requestObject);
+
+  return res.status(200).send(response.json());
 });
 
 app.listen(PORT, () => {
