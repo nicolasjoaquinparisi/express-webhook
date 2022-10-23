8219a6b7cbece5dd8a5a6f3a327db58582a60c55
Nicolás Joaquín Parisi <78542476+NicolasJoaquinParisi@users.noreply.github.com>
2022-10-23 00:02:14 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+const PORT = process.env.PORT || 3000;
+const TARGET = process.env.TARGET;
+
+app.get('/hook/healthcheck', (req, res) => {
+    res.status(200).json({msg: 'Webhook online'});
+});
+
+app.post('/hook', async (req, res) => {
+    if (!TARGET) return res.status('400').send({ msg: 'There is not target url setted'});
+
+    const payload = { ...req.body };
+
+    const response = await fetch(TARGET, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    });
+
+    return response.json();
+});
+
+app.listen(PORT, () => {
+    console.log(`Webhook online. Listening on port ${PORT}`);
+});
