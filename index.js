6d80c8d90c1030e9ef771b1b763e4d71e70b9bd8
NicolasJoaquinParisi <nicojp.1808@gmail.com>
2022-10-23 00:28:31 -0300

    added node-fetch

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const fetch = require('node-fetch');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
@@ -16,25 +17,26 @@ const PORT = process.env.PORT || 3000;
 const TARGET = process.env.TARGET;
 
 app.get('/hook/healthcheck', (req, res) => {
-    res.status(200).json({msg: 'Webhook online'});
+  res.status(200).json({ msg: 'Webhook online' });
 });
 
 app.post('/hook', async (req, res) => {
-    if (!TARGET) return res.status('400').send({ msg: 'There is not target url setted'});
+  if (!TARGET)
+    return res.status('400').send({ msg: 'There is not target url setted' });
 
-    const payload = { ...req.body };
+  const payload = { ...req.body };
 
-    const response = await fetch(TARGET, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload)
-    });
+  const response = await fetch(TARGET, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
 
-    return response.json();
+  return res.status(200).send(response);
 });
 
 app.listen(PORT, () => {
-    console.log(`Webhook online. Listening on port ${PORT}`);
+  console.log(`Webhook online. Listening on port ${PORT}`);
 });
